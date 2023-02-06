1eed7bfa4e5079417fd579902ba485179437f5b9
sheyy <sheyueyu@qq.com>
2023-02-06 21:23:50 +0800

    [openlayers05]_Downloading features（下载功能）

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import View from 'ol/View.js';
 import VectorLayer from 'ol/layer/vector';
 import VectorSource from 'ol/source/Vector';
 import { transform } from 'ol/proj';
-import Snap from 'ol/interaction/Snap';
+import Draw from 'ol/interaction/Draw';
 
 // 下面用于确定地图中心 center
 const center = [114.1692, 30.494]; //EPSG:4326
@@ -33,14 +33,39 @@ const layer = new VectorLayer({
 });
 map.addLayer(layer); //将图层加入到map中
 
-/**
- * 上面除了导入，其他和之前的没有什么不同
- *  - 添加一个交互`Snap`
- *  - 让Snap 与vector source一起工作
- *  - 将Snap 添加到map中
- */
-map.addInteraction({
-    new Snap({
-        source:source,
+map.addInteraction(
+    new Draw({
+      type:'Polygon', 
+      source: source,
     })
+  );
+
+/**--------实现清除features功能------------------
+ * 
+ *      - vector source有一个source.clear()方法。
+ *      - 我们希望点击Clear按钮就能调用source.clear()
+ */
+const clear = document.getElementById('clear');
+// 给clear按钮添加一个监听器
+clear.addEventListener('click',function(){
+    source.clear();
+});
+
+/**-----------实现download功能
+ * 
+ * - 为了序列化(serialize)我们的feature data，我们将用GeoJSON格式
+ * - 每次source中`change` 事件发生时，我们将序列化features，并为锚元素的`herf`属性构造一个数据URL
+ */
+const format = new GeoJSON({featureProjection:'EPSG:3857'});//`format`变量是一个Geojson对象
+const download = document.getElementById('download');
+
+//  - 给source绑定一个"change"事件处理器
+//  - 当source发生变化时，"change"事件处理器会被触发，并将source中所有的要素写入GeoJSON
+//  - 最后，下载链接设置为GeoJSON格式数据
+
+source.on('change',function(){
+   const features = source.getFeatures();//获取features中的所有要素；
+   const json = format.writeFeatures(features) ; //format是一个GeoJSON对象，将要素写入GeoJSON
+   download.href =
+   'data:application/json;charset=utf-8,' + encodeURIComponent(json); 
 });
